0351a312557da62eb4f18fdf4d24a280be9fd8e5
Amrutasai <46530658+Amrutasai@users.noreply.github.com>
2021-05-23 22:12:53 +0530

    CORS

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, Response
 from flask import request
 from predict_image import get_emotion
+from flask_cors import CORS
 
 # import cv2 as cv
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
